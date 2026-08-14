740f0636ad1b1c3f057e7bf7a4c2529fba8882ff
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2026-08-14 14:32:51 +0900

    chore: 스프링 부트 프로젝트 초기화

---
+++ backend/src/main/java/io/github/hayoonleeme/appointmentplatform/AppointmentPlatformApplication.java
@@ -0,0 +1,13 @@
+package io.github.hayoonleeme.appointmentplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentPlatformApplication.class, args);
+	}
+
+}
